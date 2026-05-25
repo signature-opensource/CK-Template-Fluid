0f0c8fa399c1d0175296e9c6e953745ca9972f76
Arthur Cheng <arthur.cheng@signature.one>
2026-05-25 16:02:51 +0200

    test: scaffold CK.Template.Fluid.Tests project

---
+++ Tests/CK.Template.Fluid.Tests/Usings.cs
@@ -0,0 +1,10 @@
+global using CK.Core;
+global using CK.Template.Fluid;
+global using NUnit.Framework;
+global using Shouldly;
+global using System;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
